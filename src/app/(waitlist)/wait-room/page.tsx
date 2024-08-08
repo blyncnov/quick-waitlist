cf5075b3111a60959d0aafbae2538f48a181027e
blyncnov <bholuwatife00@gmail.com>
2024-08-08 07:18:33 +0100

    quick-waitlist --> room-id to show a specific user on the waitlist

--- src/app/(waitlist)/wait-room/page.tsx
+++ src/app/(waitlist)/wait-room/page.tsx
@@ -13,7 +13,7 @@ const WaitRoom = async () => {
   return (
     <div className="max-w-6xl mx-auto py-6 flex flex-col gap-5">
       <div>
-        <h2 className="text-3xl">Your WaitRoom ({waitlist_count})</h2>
+        <h2 className="text-3xl">Your Waitlist Room ({waitlist_count})</h2>
         <p className="text-base mt-1">
           These are all the users who signed up for your waitlist.
         </p>
